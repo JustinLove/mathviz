ce690dad2c19b3772335e7b3354fc4027a2905b4
Justin Love <hg@JustinLove.name>
2009-02-06 08:49:52 -0600

    calculating old/new positions

--- matlat.rb
+++ matlat.rb
@@ -108,7 +108,16 @@ count = input 60
 unit = input 1
 calcUnit = input 1000
 timeMultiplier = input 1
-delta = input 0.1
+time = input 859294
+ms = time + 500
+
+root_position = time / calcUnit
+unit_position = root_position / unit
+un_position = unit_position / count
+
+root_to = ms / calcUnit
+unit_to = root_to / unit
+un_to = unit_to / count
 
 relativeTime = count * unit * calcUnit
 perimeter = size * scale * radians
@@ -117,6 +126,7 @@ tick = relativeTime * pixel
 realTime = calcUnit.min(tick.max(1000))
 delay = realTime / timeMultiplier
 threashold = delay * 2
+delta = un_to - un_position
 timeDelta = relativeTime * delta
 
 

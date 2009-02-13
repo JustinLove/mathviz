b8e6643b207c951e7e47d8a60797328a45211a8b
Justin Love <hg@JustinLove.name>
2009-02-12 20:21:50 -0600

    first graph, in new system

---
+++ first.rb
@@ -0,0 +1,34 @@
+require 'matlat.rb'
+
+MatLat.new('first') {
+  pi = const 3.14159
+
+  scale = input 1.0
+  size = const 172
+  count = const 60
+  unit = const 1
+  calcUnit = const 1000
+  timeMultiplier = const 1
+  time = input 859294
+  ms = time + 500
+
+  root_position = time / calcUnit
+  unit_position = root_position / unit
+  un_position = unit_position / count
+
+  root_to = ms / calcUnit
+  unit_to = root_to / unit
+  un_to = unit_to / count
+
+  relativeTime = count * unit * calcUnit
+  perimeter = size * scale * pi
+  pixel = const(1) / perimeter
+  tick = relativeTime * pixel
+  realTime = calcUnit.min(tick.max(1000))
+  delay = realTime / timeMultiplier
+  threashold = delay * 2
+  delta = un_to - un_position
+  timeDelta = relativeTime * delta
+  
+  binding
+}.dot

393e7df78a6dcf57c1c11ebf0568a081ed701967
Justin Love <hg@JustinLove.name>
2009-12-14 11:15:02 -0600

    extract common tests
    --HG--
    branch : units

--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -2,30 +2,35 @@ require 'rubygems'
 require 'spec'
 require File.join(File.dirname(__FILE__), '..', 'matlat')
 
+shared_examples_for "common combinations" do
+  it "can be created" do
+    @unit.should_not be_nil
+  end
+
+  it "rejects a mismatch" do
+    lambda {@unit + Unit.new(:x)}.should raise_error
+  end
+
+  it "adds with same" do
+    (@unit + @unit).to_s.should == @unit.to_s
+  end
+
+  it "multiplies with nothing" do
+    (@unit * Unit.new).to_s.should == @unit.to_s
+  end
+
+end
+
 describe "Unit" do
   context "with no argument" do
     before do
       @unit = Unit.new
     end
 
-    it "can be created" do
-      @unit.should_not be_nil
-    end
+    it_should_behave_like "common combinations"
 
     it "has a blank representation" do
       @unit.to_s.should == ''
-    end
-
-    it "rejects a mismatch" do
-      lambda {@unit + Unit.new(:s)}.should raise_error
-    end
-
-    it "adds with same" do
-      (@unit + @unit).to_s.should == @unit.to_s
-    end
-
-    it "multiplies with nothing" do
-      (@unit * Unit.new).to_s.should == @unit.to_s
     end
   end
 
@@ -34,24 +39,10 @@ describe "Unit" do
       @unit = Unit.new(:s)
     end
 
-    it "can be created" do
-      @unit.should_not be_nil
-    end
+    it_should_behave_like "common combinations"
 
     it "has a simple representation" do
       @unit.to_s.should == "s"
-    end
-
-    it "rejects a mismatch" do
-      lambda {@unit + Unit.new(:h)}.should raise_error
-    end
-
-    it "adds with same" do
-      (@unit + @unit).to_s.should == @unit.to_s
-    end
-
-    it "multiplies with nothing" do
-      (@unit * Unit.new).to_s.should == @unit.to_s
     end
   end
 
@@ -60,25 +51,12 @@ describe "Unit" do
       @unit = Unit.new(:s => 1)
     end
 
-    it "can be created" do
-      @unit.should_not be_nil
-    end
+    it_should_behave_like "common combinations"
 
     it "has a simple representation" do
       @unit.to_s.should == "s"
     end
 
-    it "rejects a mismatch" do
-      lambda {@unit + Unit.new(:h)}.should raise_error
-    end
-
-    it "adds with same" do
-      (@unit + @unit).to_s.should == @unit.to_s
-    end
-
-    it "multiplies with nothing" do
-      (@unit * Unit.new).to_s.should == @unit.to_s
-    end
   end
 
   context "with a denominator argument" do
@@ -86,24 +64,10 @@ describe "Unit" do
       @unit = Unit.new(:s => -1)
     end
 
-    it "can be created" do
-      @unit.should_not be_nil
-    end
+    it_should_behave_like "common combinations"
 
     it "has a 1 in the numerator position" do
       @unit.to_s.should == "1/s"
-    end
-
-    it "rejects a mismatch" do
-      lambda {@unit + Unit.new(:s)}.should raise_error
-    end
-
-    it "adds with same" do
-      (@unit + @unit).to_s.should == @unit.to_s
-    end
-
-    it "multiplies with nothing" do
-      (@unit * Unit.new).to_s.should == @unit.to_s
     end
   end
 
@@ -112,20 +76,10 @@ describe "Unit" do
       @unit = Unit.new(:V => 1, :A => 1, :h => -1)
     end
 
+    it_should_behave_like "common combinations"
+
     it "has a complex representation" do
-      @unit.to_s.should == "A*V/h"
-    end
-
-    it "rejects a mismatch" do
-      lambda {@unit + Unit.new(:s)}.should raise_error
-    end
-
-    it "adds with same" do
-      (@unit + @unit).to_s.should == @unit.to_s
-    end
-
-    it "multiplies with nothing" do
-      (@unit * Unit.new).to_s.should == @unit.to_s
+      ["V*A/h", "A*V/h"].should include @unit.to_s
     end
   end
 end

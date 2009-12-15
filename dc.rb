63832a3bce4f5e78d0239342b9cc86af88563604
Justin Love <hg@JustinLove.name>
2009-12-14 23:43:49 -0600

    update dc example with possible units

--- dc.rb
+++ dc.rb
@@ -1,17 +1,23 @@
 require 'matlat'
 
+module Units
+  new_units :ms, :s, :frame
+  new_units :rev, :xx
+  new_units :pixel, :timel
+end
+
 MatLat.new('dc') {
   pi = const 3.14159
-  second = const 1000
+  second = 1000.ms.per.s
 
   scale = input 1
-  diameter = const 172
-  count = const 10
-  unit = const 1
-  resolution = const 100
+  diameter = 172.pixel
+  count = 10.xx.per.rev
+  unit = 1.per.xx
+  resolution = 100.ms
   timeMultiplier = const 1
-  time = input((Time.now.to_f * 1000).floor)
-  frameRate = input 1
+  time = input((Time.now.to_f * 1000).floor).ms
+  frameRate = input 1.frame.per.s
 
   root_position = time / resolution
   unit_position = root_position / unit
@@ -19,19 +25,19 @@ MatLat.new('dc') {
 
   timels = unit * count
   ms_rev = resolution * timels
-  rev_s = const(1000) / ms_rev
+  rev_s = const(1000.ms.per.s) / ms_rev
   real_rev_s = rev_s * timeMultiplier
   rev_timel = const(1) / timels
   configPerimeter = diameter * pi
   perimeter = configPerimeter * scale
-  rev_pixel = const(1) / perimeter
-  tick = rev_timel.min(rev_pixel.max(rev_s))
+  rev_pixel = const(1.rev) / perimeter
+  tick = rev_timel.min((rev_pixel * 1.pixel).max(rev_s * 1.s))
   threshold = tick * 2
   real_ms_rev = ms_rev / timeMultiplier
   delay = tick * real_ms_rev
 
   step = second / frameRate
-  passed = step * timeMultiplier
+  passed = step * (timeMultiplier * 1.frame)
   #passed = delay * timeMultiplier
   ms = time + passed
 
@@ -42,9 +48,9 @@ MatLat.new('dc') {
   delta = un_to - un_position
   big = delta > threshold
   visible = delta > rev_pixel
-  animatable = real_rev_s < 0.01
+  animatable = real_rev_s < 0.01.rev.per.s
   jump = big & animatable
-  superfast = real_rev_s > 1.5
+  superfast = real_rev_s > 1.5.rev.per.s
 
   binding
 }.dot

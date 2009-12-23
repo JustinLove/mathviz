d428a12a3944937363e3d14248aaf961115e67d4
Justin Love <hg@JustinLove.name>
2009-12-22 22:08:22 -0600

    E=mc2 example

---
+++ examples/E_mc2.rb
@@ -0,0 +1,16 @@
+require 'example_helper'
+
+module Units
+  new_units :m, :s, :kg, :lb, :joule
+end
+
+MathViz.new('E_mc2') {
+
+  m = 140.lb * 0.45359237.kg.per.lb
+  c = 299_792_458.m.per.s
+  # capitalized values are constants in ruby, so we need the underscore
+  _E = (m * (c * c)) * 1.joule.per.kg.m.m.per.s.s
+
+  binding
+}.dot
+
